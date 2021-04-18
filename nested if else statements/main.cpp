0d9ef92f27515d98441a6981e132a089960fc78d
Mfundo-debug <71297224+Mfundo-debug@users.noreply.github.com>
2021-04-18 15:28:39 +0200

    Add files via upload

---
+++ nested if else statements/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+//program to find if an integer is even or odd or neither zero
+//using nested  if statements
+int main()
+{
+     int num;
+
+    cout << "Enter an integer: ";
+     cin >> num;
+
+    // outer if condition
+    if (num != 0) {
+
+        // inner if condition
+        if ((num % 2) == 0) {
+            cout << "The number is even." << endl;
+        }
+         // inner else condition
+        else {
+            cout << "The number is odd." << endl;
+        }
+    }
+    // outer else condition
+    else {
+        cout << "The number is 0 and it is neither even nor odd." << endl;
+    }
+    cout << "This line is always printed." << endl;
+
+    return 0;
+}
---
+++ program to prints 1to 5 for loop/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+//program to print from 1 to 5
+int main()
+{
+    for(int i =1; i <=5; ++i ){
+        //body loop
+        cout<<"Hello WOrld"<<endl;
+    }
+    return 0;
+}

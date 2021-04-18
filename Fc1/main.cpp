93b293b8a0d743ca0888619a09dd0c64a3bf1871
Mfundo-debug <71297224+Mfundo-debug@users.noreply.github.com>
2021-04-18 15:23:11 +0200

    Add files via upload

---
+++ Fc1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+//Program that displays a Text
+
+//declaring a function
+void greet(){
+cout<<"Hello there!";
+cout<<5+4<<endl;
+}
+int main()
+{
+    //calling a function
+    greet();
+    return 0;
+}
